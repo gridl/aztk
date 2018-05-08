04ffd6c0f6ee03df858742f3fdb7bf1c23094337
Timothee Guerin <tiguerin@microsoft.com>
2018-05-08 12:36:43 -0700

    Secrets use new models

--- aztk/models/__init__.py
+++ aztk/models/__init__.py
@@ -3,5 +3,5 @@ from .custom_script import CustomScript
 from .file_share import FileShare
 from .toolkit import TOOLKIT_MAP, Toolkit
 from .user_configuration import UserConfiguration
-
+from .secrets_configuration import SecretsConfiguration, ServicePrincipalConfiguration, SharedKeyConfiguration, DockerConfiguration
 from .models import *
--- aztk/models/models.py
+++ aztk/models/models.py
@@ -1,12 +1,14 @@
 import io
 from typing import List
+
 import azure.batch.models as batch_models
+
 from aztk import error
-from aztk.utils import helpers, deprecate
-from aztk.models.plugins import PluginConfiguration
-from aztk.internal import ConfigurationBase
-from .toolkit import Toolkit
 from aztk.core.models import Model, fields
+from aztk.internal import ConfigurationBase
+from aztk.models.plugins import PluginConfiguration
+from aztk.utils import deprecate, helpers
+
 
 class File:
     def __init__(self, name: str, payload: io.StringIO):
@@ -19,114 +21,6 @@ class RemoteLogin:
         self.ip_address = ip_address
         self.port = port
 
-
-class ServicePrincipalConfiguration(ConfigurationBase):
-    """
-    Container class for AAD authentication
-    """
-
-    def __init__(self,
-                 tenant_id: str = None,
-                 client_id: str = None,
-                 credential: str = None,
-                 batch_account_resource_id: str = None,
-                 storage_account_resource_id: str = None):
-        self.tenant_id = tenant_id
-        self.client_id = client_id
-        self.credential = credential
-        self.batch_account_resource_id = batch_account_resource_id
-        self.storage_account_resource_id = storage_account_resource_id
-
-    def validate(self) -> bool:
-        """
-        Validate the config at its current state.
-        Raises: Error if invalid
-        """
-        self._validate_required([
-            "tenant_id",
-            "client_id",
-            "credential",
-            "batch_account_resource_id",
-            "storage_account_resource_id",
-        ])
-
-
-class SharedKeyConfiguration(ConfigurationBase):
-    """
-    Container class for shared key authentication
-    """
-
-    def __init__(self,
-                 batch_account_name: str = None,
-                 batch_account_key: str = None,
-                 batch_service_url: str = None,
-                 storage_account_name: str = None,
-                 storage_account_key: str = None,
-                 storage_account_suffix: str = None):
-
-        self.batch_account_name = batch_account_name
-        self.batch_account_key = batch_account_key
-        self.batch_service_url = batch_service_url
-        self.storage_account_name = storage_account_name
-        self.storage_account_key = storage_account_key
-        self.storage_account_suffix = storage_account_suffix
-
-    def validate(self) -> bool:
-        """
-        Validate the config at its current state.
-        Raises: Error if invalid
-        """
-        self._validate_required([
-            "batch_account_name",
-            "batch_account_key",
-            "batch_service_url",
-            "storage_account_name",
-            "storage_account_key",
-            "storage_account_suffix",
-        ])
-
-
-class DockerConfiguration(ConfigurationBase):
-    def __init__(self, endpoint=None, username=None, password=None):
-
-        self.endpoint = endpoint
-        self.username = username
-        self.password = password
-
-    def validate(self):
-        pass
-
-
-class SecretsConfiguration(ConfigurationBase):
-    def __init__(self,
-                 service_principal=None,
-                 shared_key=None,
-                 docker=None,
-                 ssh_pub_key=None,
-                 ssh_priv_key=None):
-        self.service_principal = service_principal
-        self.shared_key = shared_key
-        self.docker = docker
-
-        self.ssh_pub_key = ssh_pub_key
-        self.ssh_priv_key = ssh_priv_key
-
-    def validate(self):
-        if self.service_principal and self.shared_key:
-            raise error.AztkError(
-                "Both service_principal and shared_key auth are configured, must use only one"
-            )
-        elif self.service_principal:
-            self.service_principal.validate()
-        elif self.shared_key:
-            self.shared_key.validate()
-        else:
-            raise error.AztkError(
-                "Neither service_principal and shared_key auth are configured, must use only one"
-            )
-
-    def is_aad(self):
-        return self.service_principal is not None
 
 
 class VmImage:
