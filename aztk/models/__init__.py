7828145d04252ea794ef82fc6faefd5db6145b5e
Timothee Guerin <tiguerin@microsoft.com>
2018-05-08 11:31:37 -0700

    update Cluster configuration

--- aztk/models/__init__.py
+++ aztk/models/__init__.py
@@ -1,2 +1,7 @@
-from .toolkit import Toolkit, TOOLKIT_MAP
+from .cluster_configuration import ClusterConfiguration
+from .custom_script import CustomScript
+from .file_share import FileShare
+from .toolkit import TOOLKIT_MAP, Toolkit
+from .user_configuration import UserConfiguration
+
 from .models import *
